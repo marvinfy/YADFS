2fd2ec8ec8e83f0e3a04f19a08de3e0230321277
Marcus Vinícius Neves Silva <marcusviniciusns@gmail.com>
2011-03-09 01:14:20 +0000

    Initial structure.

---
+++ client/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: marcusviniciusns
+ *
+ * Created on March 8, 2011, 10:10 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
