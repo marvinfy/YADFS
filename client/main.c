6bf458935fea08a6e824e6b258c90fd40d68966b
Marcus Vinícius Neves Silva <marcusviniciusns@gmail.com>
2011-03-21 04:09:31 +0000

    First FUSE tests.

--- client/main.c
+++ client/main.c
@@ -24,11 +24,19 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+
+static const char* temp = "/home/marcusviniciusns/temp";
+
 static int yadfs_getattr(const char *path, struct stat *stbuf)
 {
   int res;
 
-  res = lstat(path, stbuf);
+  char path_new[255];
+  memset(path_new, 0, 255);
+  memcpy(path_new, temp, strlen(temp));
+  memcpy(&(path_new[strlen(temp)]), path, strlen(path));
+
+  res = lstat(path_new, stbuf);
 
   if (res == -1)
   {
@@ -42,40 +50,62 @@ static int yadfs_getattr(const char *path, struct stat *stbuf)
 static int yadfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 		       off_t offset, struct fuse_file_info *fi)
 {
-	DIR *dp;
-	struct dirent *de;
+  DIR *dp;
+  struct dirent *de;
 
-	(void) offset;
-	(void) fi;
+  (void) offset;
+  (void) fi;
 
-	dp = opendir(path);
-	if (dp == NULL)
-		return -errno;
+  char path_new[255];
+  memset(path_new, 0, 255);
+  memcpy(path_new, temp, strlen(temp));
+  memcpy(&(path_new[strlen(temp)]), path, strlen(path));
 
-	while ((de = readdir(dp)) != NULL) {
-		struct stat st;
-		memset(&st, 0, sizeof(st));
-		st.st_ino = de->d_ino;
-		st.st_mode = de->d_type << 12;
-		if (filler(buf, de->d_name, &st, 0))
-			break;
-	}
+  dp = opendir(path_new);
 
-	closedir(dp);
-	return 0;
+  if (dp == NULL)
+  {
+    return -errno;
+  }
+
+  while ((de = readdir(dp)) != NULL) {
+    struct stat st;
+    memset(&st, 0, sizeof(st));
+    st.st_ino = de->d_ino;
+    st.st_mode = de->d_type << 12;
+    if (filler(buf, de->d_name, &st, 0))
+    {
+      break;
+    }
+  }
+
+  closedir(dp);
+  return 0;
 }
 
+
 static struct fuse_operations yadfs_operations = {
-    .getattr   = yadfs_getattr,
-    .readdir   = yadfs_readdir
+  .getattr   = yadfs_getattr,
+  .readdir   = yadfs_readdir
 };
 
-/*
- * 
- */
+static int init()
+{
+  //sleep(5);
+  return 1;
+}
+
 int main(int argc, char** argv)
 {
   int ret;
+
+  ret = init();
+  
+  if (ret < 0)
+  {
+    return ret;
+  }
+
   ret = fuse_main(argc, argv, &yadfs_operations, NULL);
 
   return ret;
